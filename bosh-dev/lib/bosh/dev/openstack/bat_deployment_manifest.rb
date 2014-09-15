ca8462fcfd35fca2d9fbe85e13a963bff410d8e4
Zachary Auerbach <zauerbach@pivotal.io>
2014-09-15 16:29:06 -0700

    wip
    Signed-off-by: Adam Stegman <astegman@pivotal.io>

--- bosh-dev/lib/bosh/dev/openstack/bat_deployment_manifest.rb
+++ bosh-dev/lib/bosh/dev/openstack/bat_deployment_manifest.rb
@@ -20,7 +20,26 @@ module Bosh::Dev::Openstack
         'cpi' => 'openstack',
         'properties' => {
           'vip' => env['BOSH_OPENSTACK_VIP_BAT_IP'],
-          'static_ip' => env['BOSH_OPENSTACK_STATIC_BAT_IP'],
+          'networks' => [
+            {
+              'name' => 'default',
+              'static_ip' => env['BOSH_OPENSTACK_STATIC_BAT_IP_0'],
+              'type' => net_type,
+              'cloud_properties' => {
+                'net_id' => env['BOSH_OPENSTACK_NET_ID_0'],
+                'security_groups' => ['default']
+              },
+            },
+            {
+              'name' => 'second',
+              'static_ip' => env['BOSH_OPENSTACK_STATIC_BAT_IP_1'],
+              'type' => net_type,
+              'cloud_properties' => {
+                'net_id' => env['BOSH_OPENSTACK_NET_ID_1'],
+                'security_groups' => ['default']
+              }
+            },
+          ],
           'second_static_ip' => env['BOSH_OPENSTACK_SECOND_STATIC_BAT_IP'],
           'uuid' => director_uuid.value,
           'pool_size' => 1,
@@ -30,13 +49,6 @@ module Bosh::Dev::Openstack
           },
           'instances' => 1,
           'mbus' => "nats://nats:0b450ada9f830085e2cdeff6@#{env['BOSH_OPENSTACK_VIP_BAT_IP']}:4222",
-          'network' => {
-            'type' => net_type,
-            'cloud_properties' => {
-              'net_id' => env['BOSH_OPENSTACK_NET_ID'],
-              'security_groups' => ['default']
-            }
-          }
         }
       }
 
@@ -45,13 +57,15 @@ module Bosh::Dev::Openstack
         manifest_hash['properties']['key_name'] = key_name
       end
 
-      if net_type == 'manual'
-        manifest_hash['properties']['network'].merge!(
-          'cidr' => env['BOSH_OPENSTACK_NETWORK_CIDR'],
-          'reserved' => [env['BOSH_OPENSTACK_NETWORK_RESERVED']],
-          'static' => [env['BOSH_OPENSTACK_NETWORK_STATIC']],
-          'gateway' => env['BOSH_OPENSTACK_NETWORK_GATEWAY']
-        )
+      manifest_hash['properties']['networks'].each_with_index do |network, i|
+        if net_type == 'manual'
+          network.merge!(
+            'cidr' => env["BOSH_OPENSTACK_NETWORK_CIDR_#{i}"],
+            'reserved' => [env["BOSH_OPENSTACK_NETWORK_RESERVED_#{i}"]],
+            'static' => [env["BOSH_OPENSTACK_NETWORK_STATIC_#{i}"]],
+            'gateway' => env["BOSH_OPENSTACK_NETWORK_GATEWAY_#{i}"]
+          )
+        end
       end
 
       manifest_hash
