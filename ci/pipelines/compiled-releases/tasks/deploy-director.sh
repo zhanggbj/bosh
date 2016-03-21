e9a6096b9933a63dd0a6760f0bfc1fc931b703e4
Tyler Schultz <tschultz@pivotal.io>
2016-03-21 11:15:55 -0700

    Move aws configuration to deploy task.
    chmod the ssh key, specify with different yml format.
    
    Signed-off-by: Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>

--- ci/pipelines/compiled-releases/tasks/deploy-director.sh
+++ ci/pipelines/compiled-releases/tasks/deploy-director.sh
@@ -2,6 +2,7 @@
 
 $PWD/bosh-src/ci/pipelines/compiled-releases/tasks/generate-bosh-init-manifest.sh > bosh-init.yml
 
-echo $BOSH_SSH_TUNNEL_KEY > ssh_tunnel_key
+echo "$BOSH_SSH_TUNNEL_KEY" > ssh_tunnel_key
+chmod 600 ssh_tunnel_key
 
 bosh-init deploy bosh-init.yml
