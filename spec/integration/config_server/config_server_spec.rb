bd7d99a6734d9fc721e173e9231e44c7bf34fd58
Andrew Su <asu@pivotal.io>
2016-07-08 16:49:12 -0400

    director interpolates runtime config.
    [#119700377]
    https://www.pivotaltracker.com/story/show/119700377
    
    Signed-off-by: Vikram Yadav <vyadav@pivotal.io>

--- spec/integration/config_server/config_server_spec.rb
+++ spec/integration/config_server/config_server_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'using director with config server', type: :integration do
 
+  let (:manifest_hash) { Bosh::Spec::Deployments.simple_manifest }
+  let (:cloud_config)  { Bosh::Spec::Deployments.simple_cloud_config }
+  let(:config_server_helper) { Bosh::Spec::ConfigServerHelper.new(current_sandbox.port_provider.get_port(:config_server_port)) }
+
   context 'when config server cretificates are not trusted' do
     with_reset_sandbox_before_each(config_server_enabled: true, with_config_server_trusted_certs: false)
 
@@ -11,9 +15,6 @@ describe 'using director with config server', type: :integration do
     end
 
     it 'throws certificate validator error' do
-      manifest_hash = Bosh::Spec::Deployments.simple_manifest
-      cloud_config = Bosh::Spec::Deployments.simple_cloud_config
-
       manifest_hash['jobs'].first['properties'] = {'test_property' => '((test_property))'}
       output, exit_code = deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config, failure_expected: true, return_exit_code: true)
 
@@ -23,7 +24,7 @@ describe 'using director with config server', type: :integration do
 
   end
 
-  context 'when config server cretificates are trusted' do
+  context 'when config server certificates are trusted' do
     with_reset_sandbox_before_each(config_server_enabled: true)
 
     before do
@@ -33,9 +34,6 @@ describe 'using director with config server', type: :integration do
 
     context 'when deployment manifest has placeholders' do
       it 'raises an error when config server does not have values for placeholders' do
-        manifest_hash = Bosh::Spec::Deployments.simple_manifest
-        cloud_config = Bosh::Spec::Deployments.simple_cloud_config
-
         manifest_hash['jobs'].first['properties'] = {'test_property' => '((test_property))'}
         output, exit_code = deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config, failure_expected: true, return_exit_code: true)
 
@@ -44,10 +42,7 @@ describe 'using director with config server', type: :integration do
       end
 
       it 'replaces placeholders in the manifest when config server has value for placeholders' do
-        config_server_helper = Bosh::Spec::ConfigServerHelper.new(current_sandbox.port_provider.get_port(:config_server_port))
         config_server_helper.put_value('test_property', 'cats are happy')
-        manifest_hash = Bosh::Spec::Deployments.simple_manifest
-        cloud_config = Bosh::Spec::Deployments.simple_cloud_config
 
         manifest_hash['jobs'].first['properties'] = {'test_property' => '((test_property))'}
         deploy_from_scratch(manifest_hash: manifest_hash, cloud_config_hash: cloud_config)
@@ -55,6 +50,18 @@ describe 'using director with config server', type: :integration do
 
         template = vm.read_job_template('foobar', 'bin/foobar_ctl')
         expect(template).to include('test_property=cats are happy')
+      end
+    end
+
+    context 'when runtime manifest has placeholders' do
+      it 'replaces placeholders in the addons' do
+        config_server_helper.put_value('release_name', 'dummy2')
+
+        Dir.mktmpdir do |tmpdir|
+          runtime_config_filename = File.join(tmpdir, 'runtime_config.yml')
+          File.write(runtime_config_filename, Psych.dump(Bosh::Spec::Deployments.runtime_config_with_addon_placeholders))
+          expect(bosh_runner.run("update runtime-config #{runtime_config_filename}")).to include("Successfully updated runtime config")
+        end
       end
     end
   end
