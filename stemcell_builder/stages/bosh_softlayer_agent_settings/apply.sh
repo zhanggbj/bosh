80866c1b861af86e3c444b7a0da739d15fbfbf85
jianqiu <wangjq@cn.ibm.com>
2016-07-18 23:32:22 +0800

    bump bosh-agent for baremetal

--- stemcell_builder/stages/bosh_softlayer_agent_settings/apply.sh
+++ stemcell_builder/stages/bosh_softlayer_agent_settings/apply.sh
@@ -8,7 +8,8 @@ cat > $chroot/var/vcap/bosh/agent.json <<JSON
 {
   "Platform": {
     "Linux": {
-      "CreatePartitionIfNoEphemeralDisk": true
+      "CreatePartitionIfNoEphemeralDisk": false,
+      "SkipDiskSetup": true
     }
   },
   "Infrastructure": {
